2256d4c71aced51f80eee7f7f813f423fc21c950
Fredrick Garingo <fredrickjohng6@gmail.com>
2020-09-08 11:20:22 +0800

    now ready

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import HomePage from './pages'
 import AboutPage from './pages/about'
 import ContactPage from './pages/contact'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <Router>
+      <Navbar/>
+      <Route exact path="/" component={HomePage}/>
       <Switch>
-        <Route exact path="/" component={HomePage}/>
         <Route path="/about" component={AboutPage}/>
         <Route path="/contact" component={ContactPage}/>
       </Switch>
