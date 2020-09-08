23f5e187af0d94bdc9f97c62263b175b3dd77b77
Fredrick Garingo <fredrickjohng6@gmail.com>
2020-09-08 11:45:14 +0800

    prettier

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,19 +11,15 @@ const Navbar = () => {
     return (
         <>
         <Menu>
-        <Link id="home" className="h1-responsive font-weight-bold brand pb-4" to="/">Brand</Link>
-
+            <Link id="home" className="h1-responsive font-weight-bold brand pb-4" to="/">Brand</Link>
             <span>
-            <Link id="about" className="menu-item h2-responsive" to="/about">About Us</Link>
-
+                <Link id="about" className="menu-item h2-responsive" to="/about">About Us</Link>
             </span>
             <span>
-            <Link id="story" className="menu-item h2-responsive" to="/story">Our Story</Link>
-
+                <Link id="story" className="menu-item h2-responsive" to="/story">Our Story</Link>
             </span>
             <span>
-            <Link id="contact" className="menu-item h2-responsive" to="/contact">Contact</Link>
-
+                <Link id="contact" className="menu-item h2-responsive" to="/contact">Contact</Link>
             </span>
             {/* <a onClick={showSettings} className="menu-item--small" href="">Settings</a> */}
         </Menu>
