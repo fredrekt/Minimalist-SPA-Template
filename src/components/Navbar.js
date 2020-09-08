2fbb7359f9ace7f05afcc9b8c98524a0a0817954
Fredrick Garingo <fredrickjohng6@gmail.com>
2020-09-08 10:40:33 +0800

    navbar done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,33 @@
 import React from 'react'
+import { slide as Menu } from 'react-burger-menu'
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+
+    // const showSettings = e => {
+    //     e.preventDefault();
+    // }
+
     return (
-        <div>
-            
-        </div>
+        <>
+        <Menu>
+        <Link id="home" className="h1-responsive font-weight-bold brand pb-4" to="/">Brand</Link>
+
+            <span>
+            <Link id="about" className="menu-item h2-responsive" to="/about">About Us</Link>
+
+            </span>
+            <span>
+            <Link id="story" className="menu-item h2-responsive" to="/story">Our Story</Link>
+
+            </span>
+            <span>
+            <Link id="contact" className="menu-item h2-responsive" to="/contact">Contact</Link>
+
+            </span>
+            {/* <a onClick={showSettings} className="menu-item--small" href="">Settings</a> */}
+        </Menu>
+        </>
     )
 }
 
