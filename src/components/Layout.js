57322c2bee985497add85ea2dfa03651ba85f05e
Fredrick Garingo <fredrickjohng6@gmail.com>
2020-09-09 10:11:29 +0800

    parent layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import FooterPage from './Footer'
 
 const Layout = ({children}) => {
     return (
         <div className="py-5 position-absolute w-100 h-100 my-5">
             <main>{children}</main>
+            <FooterPage/>
         </div>
     )
 }
